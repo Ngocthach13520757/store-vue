7b28c87caa16a9b850c95e4039f79f2ee9b7bfe0
thachhn3 <thachhn3@vng.com.vn>
2019-04-30 14:47:29 +0700

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="title">{{msg}}</div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      msg: "Welcome to home page"
+    };
+  }
+};
+</script>
